77868bccc8acf59ddfa23d272486de9029ee7535
ebelious <belanger.eric.m@gmail.com>
2024-09-01 18:52:48 -0400

    Create pdef.sh

---
+++ Scripts/pdef.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#       _          _ _
+#   ___| |__   ___| (_) ___  _   _ ___
+#  / _ \ '_ \ / _ \ | |/ _ \| | | / __|
+# |  __/ |_) |  __/ | | (_) | |_| \__ \
+#  \___|_.__/ \___|_|_|\___/ \__,_|___/
+# https://github.com/ebelious
+#
+# Online Portuguese > English Dictionary
+#
+# Requires: link2 or links
+#
+
+links "https://dictionary.cambridge.org/dictionary/portuguese-english/$1"
