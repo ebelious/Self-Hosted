1daad161dbd3208bdb1bb6f0eef1e3c06fef119f
ebelious <belanger.eric.m@gmail.com>
2024-10-24 15:19:12 -0400

    Update oui.sh

--- Scripts/oui.sh
+++ Scripts/oui.sh
@@ -16,42 +16,48 @@ get_oui(){
 if [[ $1 =~ ^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$ ]]
 then
         OUI=$(echo "$1" | cut -d ':' -f 1-3 | tr '[:lower:]' '[:upper:]')
-        cat /home/ebelanger/manuf | grep $OUI
+        cat ./manuf | grep $OUI
         exit 0
 
 elif [[ $1 =~ ^([0-9A-Fa-f]{2}[-]){5}([0-9A-Fa-f]{2})$ ]]
 then
         OUI=$(echo "$1" | tr -d '-' | sed -e 's/[0-9A-Fa-f]\{2\}/&:/g' -e 's/:$//' | cut -d ':' -f 1-3 | tr '[:lower:]' '[:upper:]')
-        cat /home/ebelanger/manuf | grep $OUI
+        cat ./manuf | grep $OUI
         exit 0
 
 elif [[ $1 =~ ^([0-9A-Fa-f]{4}[.]){2}([0-9A-Fa-f]{4})$ ]]
 then
         OUI=$(echo "$1" | tr -d '.' | sed -e 's/[0-9A-Fa-f]\{2\}/&:/g' -e 's/:$//' | cut -d ':' -f 1-3 | tr '[:lower:]' '[:upper:]')
-        cat /home/ebelanger/manuf | grep $OUI
+        cat ./manuf | grep $OUI
         exit 0
 
 elif [[ $1 =~ ^([0-9A-Fa-f]{2}[:]){2}([0-9A-Fa-f]{2})$ ]]
 then
         OUI=$(echo "$1" | cut -d ':' -f 1-3 | tr '[:lower:]' '[:upper:]')
-        cat /home/ebelanger/manuf | grep $OUI
+        cat ./manuf | grep $OUI
         exit 0
 
 elif [[ $1 =~ ^([0-9A-Fa-f]{4}[.])([0-9A-Fa-f]{2})$ ]]
 then
         OUI=$(echo "$1" | tr -d '.' | sed -e 's/[0-9A-Fa-f]\{2\}/&:/g' -e 's/:$//' | cut -d ':' -f 1-3 | tr '[:lower:]' '[:upper:]')
-        cat /home/ebelanger/manuf | grep $OUI
+        cat ./manuf | grep $OUI
         exit 0
 
+elif [[ $1 =~ ^([0-9A-Fa-f]{2}[-]){2}([0-9A-Fa-f]{2})$ ]]
+then
+        OUI=$(echo "$1" | cut -d ':' -f 1-3 | tr '[:lower:]' '[:upper:]')
+        cat ./manuf | grep $OUI
+        exit 0
 else
         echo -e "\e[0:35m This is not in the correct format \e[0m"
         echo
-        echo "oui [MAC_ADDRESS] \nxx:xx:xx:xx:xx \nxx-xx-xx-xx-xx-xx \nxxxx.xxxx.xxxx"
+        echo -e "USAGE: oui [MAC_ADDRESS]\n\n Mac Address Formats: \n    xx:xx:xx:xx:xx \n    xx-xx-xx-xx-xx-xx \n    xxxx.xxxx.xxxx \n    xx:xx:xx \n    xx-xx-xx \n    xxxx.xx \n\n Example: oui 00d9.d110.21f9"
 fi
 }
 
 get_oui $1
 
 if [ $? -ne 0 ]; then
-    echo "No OUI was found."
+	echo "No OUI was found."
+	exit 1
 fi
