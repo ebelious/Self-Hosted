a31efc01e871af1035ba978866eded46ae636fc7
ebelious <belanger.eric.m@gmail.com>
2025-04-02 11:40:46 -0400

    Create toggle-waybar.sh

---
+++ Scripts/toggle-waybar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#   ___| |__   ___| (_) ___  _   _ ___
+#  / _ \ '_ \ / _ \ | |/ _ \| | | / __|
+# |  __/ |_) |  __/ | | (_) | |_| \__ \
+#  \___|_.__/ \___|_|_|\___/ \__,_|___/
+# https://github.com/ebelious
+#
+# Toggles waybar visibility
+# Requires: waybar
+ 
+# Toggles waybar with keybind in river init
+PROC=$(pidof waybar)
+if [ -z "$PROC" ]; then
+    exec waybar
+elif [ -n "$PROC" ]; then
+   pkill waybar
+fi
